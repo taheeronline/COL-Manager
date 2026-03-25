15fd8d32f74aca8aedb518e911be29c5eb8f8f38
Shamsuddin <taheeronline@gmail.com>
2026-03-25 15:57:20 +0530

    Script updated, Columns in dtase fixed. Enums fixed. HAving issue with Column Status on Cherkout window

---
+++ COLManager.Web/Migrations/20260325094843_UpdateStatus.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COLManager.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Column_Usage_Log",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Column_Usage_Log",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50);
+        }
+    }
+}
